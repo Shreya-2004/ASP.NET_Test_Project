3cee3252faa1211c55a925c9465733e2d1bbf30c
Shreya Bagde <shreyabagde2001@gmail.com>
2024-02-28 21:57:40 +0530

    Initial commit message.

---
+++ Ecommerce/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Ecommerce.Models;
+
+namespace Ecommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+     
+        // GET: Login
+        public ActionResult login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult loginform( login loginuser)
+        {
+            string email = loginuser.Email;
+            string Password =loginuser.Password;
+            if (email == "abhishek.raut@gmail.com" && Password == "admin@12345")
+            {
+                ViewBag.Message = "LOGIN SUCCESSFULL";
+            }
+            else
+            {
+                ViewBag.Message = "Please enter valid Email ID and Password";
+
+            }
+            return View();
+        }
+ }
+
+
+    
+}
