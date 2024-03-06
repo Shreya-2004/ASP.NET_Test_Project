15f992de67ab19d7d2150ae04a80f880cbb30555
Shreya Bagde <67917783+Shreya-2004@users.noreply.github.com>
2024-03-06 19:36:50 +0530

    Inheritance_overloading

---
+++ Inheritance_overloading/Inheritance_overloading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_overloading
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int Speed;
+            string Color;
+            
+
+            Speed =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Speed is: " + Speed);
+            Color =Console.ReadLine();
+            Console.WriteLine("Color is: " + Color);
+           
+            //TwoWheeler twoWheelerObj = new TwoWheeler();
+
+            //string InputSpeed = null;
+            //Console.WriteLine("Enter Speed :");
+            //InputSpeed = Console.ReadLine();
+            //twoWheelerObj.SetSpeed(InputSpeed);
+            //Console.WriteLine("Speed is: " + InputSpeed);
+
+            //string InputColor = null;
+            //Console.WriteLine("Enter Color :");
+            //InputColor = Console.ReadLine();
+            //twoWheelerObj.SetSpeed(InputColor);
+            //Console.WriteLine("Color is: " + InputColor);
+
+
+        }
+      
+    }
+}
---
+++ Inheritance_overloading/Inheritance_overloading/TwoWheeler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_overloading
+{
+    public class TwoWheeler
+    {
+
+        public string TwoWheelerSpeed;
+        public string TwoWheelerColor;
+        public string DisplaySpeed()
+        {
+            return TwoWheelerSpeed;
+        }
+        public string SetSpeed(string speed)
+        {
+            TwoWheelerSpeed = speed;
+            return TwoWheelerSpeed;
+        }
+    }
+
+}
